9d1fe3ab87775c4aa70971981cfd7acb172cfc79
MorshedNabil <nabilmorshed1251@gmail.com>
2026-02-17 00:45:59 +0600

    Firebase authentication

--- app/src/main/java/com/nabil/smartkrishi/MainActivity.java
+++ app/src/main/java/com/nabil/smartkrishi/MainActivity.java
@@ -2,14 +2,37 @@ package com.nabil.smartkrishi;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.SwitchCompat;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.FirebaseException;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.PhoneAuthCredential;
+import com.google.firebase.auth.PhoneAuthOptions;
+import com.google.firebase.auth.PhoneAuthProvider;
+
+import java.util.concurrent.TimeUnit;
+
+
 public class MainActivity extends AppCompatActivity {
+
+    TextView errorMsg;
+    EditText inputPhone;
+    SwitchCompat langSwitch;
+    Button signupButton, getOtpButton;
+    private FirebaseAuth Auth;
+    private PhoneAuthProvider.OnVerificationStateChangedCallbacks mCallBacks;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,18 +41,68 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
 
-        Button getOtpButton = findViewById(R.id.btn_get_otp);
-        Button signupButton = findViewById(R.id.btn_signup_link);
-        SwitchCompat langSwitch = findViewById(R.id.lang_switch);
+        getOtpButton = findViewById(R.id.btn_get_otp);
+        signupButton = findViewById(R.id.btn_signup_link);
+        langSwitch = findViewById(R.id.lang_switch);
+        inputPhone = findViewById(R.id.input_phone_login);
+        errorMsg = findViewById(R.id.tv_error_message);
+        // Initialize Firebase Auth
+        Auth = FirebaseAuth.getInstance();
 
         // Set an OnClickListener on the button
         getOtpButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // Call the function to navigate to the next activity
-                openOtpVerificationActivity();
+
+                String phone = inputPhone.getText().toString().trim();
+                String phoneNumber = "+880" + phone;
+
+                if(!phone.isEmpty()){
+                    PhoneAuthOptions options = PhoneAuthOptions.newBuilder(Auth)
+                            .setPhoneNumber(phoneNumber)
+                            .setTimeout(60L, TimeUnit.SECONDS)
+                            .setActivity(MainActivity.this)
+                            .setCallbacks(mCallBacks)
+                            .build();
+                    PhoneAuthProvider.verifyPhoneNumber(options);
+                }
+                else{
+                    inputPhone.setError("Please enter phone number");
+                    inputPhone.requestFocus();
+                    return;
+                }
             }
         });
+
+        mCallBacks = new PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
+            @Override
+            public void onVerificationCompleted(@NonNull PhoneAuthCredential phoneAuthCredential) {
+                signIn(phoneAuthCredential);
+            }
+
+            @Override
+            public void onVerificationFailed(@NonNull FirebaseException e) {
+                errorMsg.setVisibility(View.VISIBLE);
+                errorMsg.setText(e.getMessage());
+
+            }
+
+            @Override
+            public void onCodeSent(@NonNull String s, @NonNull PhoneAuthProvider.ForceResendingToken forceResendingToken) {
+                super.onCodeSent(s, forceResendingToken);
+
+                // Sometimes the code doesn't detect automatically
+                // Call the function to navigate to the next activity
+                new Handler().postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        Intent OTPintent = new Intent(MainActivity.this, OtpVerification.class);
+                        OTPintent.putExtra("auth", s);
+                        startActivity(OTPintent);
+                    }
+                }, 10000);
+            }
+        };
 
         signupButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -47,16 +120,22 @@ public class MainActivity extends AppCompatActivity {
 
         });
     }
-
-    /**
-     * Navigates from MainActivity to the OTP verification activity.
-     */
-    private void openOtpVerificationActivity() {
-        // Create an Intent to start the new activity
-        // Replace OtpVerificationActivity.class with the actual name of your next activity
-        Intent intent = new Intent(MainActivity.this, OtpVerification.class);
-        startActivity(intent);
+    // ========================= Sign In ======================================
+    private void signIn(PhoneAuthCredential credential) {
+        Auth.signInWithCredential(credential).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                    if(task.isSuccessful()){
+                        sendToMain();
+                    }
+                    else{
+                        errorMsg.setVisibility(View.VISIBLE);
+                        errorMsg.setText(task.getException().getMessage());
+                    }
+            }
+        });
     }
+    // ============================================================
 
     /**
      * Creates an Intent to navigate from the current activity to SignupActivity.
@@ -65,6 +144,23 @@ public class MainActivity extends AppCompatActivity {
         // Create an Intent to specify which activity to start
         Intent intent = new Intent(MainActivity.this, SignUp.class);
         startActivity(intent);
+    }
+
+//    @Override
+//    public void onStart() {
+//        super.onStart();
+//        // Check if user is signed in (non-null) and update UI accordingly.
+//        FirebaseUser currentUser = Auth.getCurrentUser();
+//
+//        if(currentUser != null){
+//            sendToMain();
+//        }
+//    }
+//
+    private void sendToMain() {
+        Intent intent = new Intent(MainActivity.this, OnBoardActivity.class);
+        startActivity(intent);
+        finish();
     }
 
 }
--- app/src/main/java/com/nabil/smartkrishi/SignUp.java
+++ app/src/main/java/com/nabil/smartkrishi/SignUp.java
@@ -1,7 +1,10 @@
 package com.nabil.smartkrishi;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
 
@@ -12,10 +15,12 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class SignUp extends AppCompatActivity {
     Spinner divisionSpinner, districtSpinner;
     EditText inputPhone, inputNid, inputVillage;
+    Button SignUpBtn;
     private FirebaseAuth mAuth;
 
     @Override
@@ -28,6 +33,8 @@ public class SignUp extends AppCompatActivity {
         inputPhone = findViewById(R.id.input_phone);
         inputNid = findViewById(R.id.input_nid);
         inputVillage = findViewById(R.id.input_village);
+        SignUpBtn = findViewById(R.id.btn_signup);
+
         // Initialize Firebase Auth
         mAuth = FirebaseAuth.getInstance();
 
@@ -47,6 +54,15 @@ public class SignUp extends AppCompatActivity {
 
         userRegister();
 
+        // ============================ Signup Button =====================================
+        SignUpBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+
+            }
+        });
+
 
     }
 
@@ -59,21 +75,38 @@ public class SignUp extends AppCompatActivity {
         String district = districtSpinner.getSelectedItem().toString().trim();
 
         if(nidNumber.isEmpty()){
-            inputNid.setError("Please enter all the fields");
+            inputNid.setError("Please enter NID number");
             inputNid.requestFocus();
             return;
         }
         else if(villageName.isEmpty()){
-            inputVillage.setError("Please enter Village fields");
+            inputVillage.setError("Please enter Village name");
             inputVillage.requestFocus();
             return;
         }
         else if(phone.isEmpty()){
-            inputPhone.setError("Please enter Phone fields");
+            inputPhone.setError("Please enter Phone Number");
             inputPhone.requestFocus();
             return;
         }
 
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+
+        if(currentUser != null){
+            sendToMain();
+        }
+    }
+
+    private void sendToMain() {
+        Intent intent = new Intent(SignUp.this, OnBoardActivity.class);
+        startActivity(intent);
+        finish();
     }
 
 }
